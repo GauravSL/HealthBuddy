9988eacc35b0d06e542c04c31d55285743b41225
GL <gl@gmail.com>
2022-05-16 00:23:51 +0530

    Server implement

--- app/src/main/java/com/example/healthbuddy/registration/user/UserRegistrationActivity.java
+++ app/src/main/java/com/example/healthbuddy/registration/user/UserRegistrationActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.healthbuddy.R;
+import com.google.android.material.imageview.ShapeableImageView;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -17,12 +20,23 @@ import java.util.Locale;
 public class UserRegistrationActivity extends AppCompatActivity {
     final Calendar myCalendar= Calendar.getInstance();
     EditText editText;
+    LinearLayout ll_password_container, main_container;
+    private ShapeableImageView img_profile;
+    private TextView tv_userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile);
         editText=(EditText) findViewById(R.id.et_userDOB);
+        ll_password_container=(LinearLayout) findViewById(R.id.password_container);
+        ll_password_container.setVisibility(View.VISIBLE);
+        main_container=(LinearLayout) findViewById(R.id.main_container);
+        main_container.setBackgroundColor(getResources().getColor(R.color.white));
+        img_profile=(ShapeableImageView) findViewById(R.id.img_profile);
+        img_profile.setVisibility(View.GONE);
+        tv_userId=(TextView) findViewById(R.id.tv_userId);
+        tv_userId.setText("Register");
         DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int day) {
